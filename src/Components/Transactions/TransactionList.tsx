b411dcc80cc5d5c021894c2de4cb25d8a7a81fcd
Armando Joergensen <armando-a-j@hotmail.com>
2022-01-31 00:55:37 -0300

    Update/Description
    Limitando a 10 itens a listagem na home

--- src/Components/Transactions/TransactionList.tsx
+++ src/Components/Transactions/TransactionList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {FlatList} from 'react-native';
-import {PropsTransaction} from '../../Home';
-import {TransactionsItem} from './TransactionItem';
+import React from "react";
+import { FlatList } from "react-native";
+import { PropsTransaction } from "../../Home";
+import { TransactionsItem } from "./TransactionItem";
 
 interface PropsTransactionsList {
   data: PropsTransaction[];
@@ -18,14 +18,17 @@ export const TransactionsList: React.FC<PropsTransactionsList> = ({
   return (
     <FlatList
       data={data}
-      keyExtractor={item => item.id}
-      renderItem={({item}) => (
-        <TransactionsItem
-          item={item}
-          selected={() => handleSelectedItem(item)}
-        />
-      )}
-      contentContainerStyle={{paddingBottom: 70}}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item, index }) => {
+        if (index > 9) return <></>;
+        return (
+          <TransactionsItem
+            item={item}
+            selected={() => handleSelectedItem(item)}
+          />
+        );
+      }}
+      contentContainerStyle={{ paddingBottom: 70 }}
     />
   );
 };
