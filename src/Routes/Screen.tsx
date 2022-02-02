4e9ea51af0bec06577b259101f5e35a0f82ab7e5
Armando Joergensen <armando-a-j@hotmail.com>
2022-02-02 09:42:52 -0300

    Update/Route

--- src/Routes/Screen.tsx
+++ src/Routes/Screen.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {MyTab} from './Tab';
-import {NewTransactionScreen} from '../NewTransaction';
-import {TransactionDescriptionScreen} from '../NewTransaction/TransactionDescription';
-import {TransactionValueScreen} from '../NewTransaction/TransactionValue';
-import {ReceiptScreen} from '../NewTransaction/Receipt';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { MyTab } from "./Tab";
+import { NewTransactionScreen } from "../NewTransaction";
+import { TransactionDescriptionScreen } from "../NewTransaction/TransactionDescription";
+import { TransactionValueScreen } from "../NewTransaction/TransactionValue";
+import { ReceiptScreen } from "../NewTransaction/Receipt";
+import { TransactionType } from "../NewTransaction/TransactionType";
 
 export interface PropsItem {
   active?: boolean;
-  createdAt?: {nanoseconds: number; seconds: number};
+  place?: "CASA" | "LOJA";
+  createdAt?: { nanoseconds: number; seconds: number };
   description?: string;
   editedAt?: Date;
   price?: number;
-  type?: 'ENTRADA' | 'SAÍDA';
+  type?: "ENTRADA" | "SAÍDA";
   id?: string;
 }
 
 export type RootStackParamList = {
   HomeStack: undefined;
-  Step1: {item: PropsItem};
-  Step2: {item: PropsItem};
-  Step3: {item: PropsItem};
-  Step4: {item: PropsItem};
+  Step: { item: PropsItem };
+  Step1: { item: PropsItem };
+  Step2: { item: PropsItem };
+  Step3: { item: PropsItem };
+  Step4: { item: PropsItem };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -31,8 +34,10 @@ export const MyStack: React.FC = () => {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-      }}>
+      }}
+    >
       <Stack.Screen name="HomeStack" component={MyTab} />
+      <Stack.Screen name="Step" component={TransactionType} />
       <Stack.Screen name="Step1" component={NewTransactionScreen} />
       <Stack.Screen name="Step2" component={TransactionDescriptionScreen} />
       <Stack.Screen name="Step3" component={TransactionValueScreen} />
